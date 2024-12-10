4594d2b4780773f8edad9bbdd6dd2d4f18978438
Ethan-Shek <dinoeshek@gmail.com>
2024-12-10 13:15:19 -0500

    start

--- IGME201_Final_Project/Form1.cs
+++ IGME201_Final_Project/Form1.cs
@@ -16,5 +16,53 @@ namespace IGME201_Final_Project
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateAcc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void accQuestion_Click(object sender, EventArgs e)
+        {
+
+       
+        }
+
+        private void LoginTitle_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginConfirm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Username_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateAcc_Click_1(object sender, EventArgs e)
+        {
+            userControl11.Show();
+            userControl21.Hide();
+            userControl21.BringToFront();
+        }
     }
 }
--- IGME201_Final_Project/login.Designer.cs
+++ IGME201_Final_Project/login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace IGME201_Final_Project
 {
-    partial class Form1
+    partial class login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.accQuestion = new System.Windows.Forms.Label();
             this.CreateAcc = new System.Windows.Forms.Button();
             this.LoginTitle = new System.Windows.Forms.Label();
@@ -37,140 +36,106 @@
             this.Username = new System.Windows.Forms.Label();
             this.Password = new System.Windows.Forms.Label();
             this.PasswordTypeBox = new System.Windows.Forms.TextBox();
-            this.userControl21 = new IGME201_Final_Project.UserControl2();
-            this.userControl11 = new IGME201_Final_Project.UserControl1();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.accQuestion);
-            this.panel1.Controls.Add(this.CreateAcc);
-            this.panel1.Controls.Add(this.LoginTitle);
-            this.panel1.Controls.Add(this.loginConfirm);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.Username);
-            this.panel1.Controls.Add(this.Password);
-            this.panel1.Controls.Add(this.PasswordTypeBox);
-            this.panel1.Location = new System.Drawing.Point(0, -2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1255, 962);
-            this.panel1.TabIndex = 2;
             // 
             // accQuestion
             // 
             this.accQuestion.AutoSize = true;
             this.accQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.accQuestion.Location = new System.Drawing.Point(433, 691);
+            this.accQuestion.Location = new System.Drawing.Point(406, 723);
             this.accQuestion.Name = "accQuestion";
             this.accQuestion.Size = new System.Drawing.Size(388, 29);
-            this.accQuestion.TabIndex = 23;
+            this.accQuestion.TabIndex = 15;
             this.accQuestion.Text = "Don\'t have an account? Create one!";
             // 
             // CreateAcc
             // 
             this.CreateAcc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.CreateAcc.Location = new System.Drawing.Point(472, 754);
+            this.CreateAcc.Location = new System.Drawing.Point(445, 786);
             this.CreateAcc.Name = "CreateAcc";
             this.CreateAcc.Size = new System.Drawing.Size(294, 53);
-            this.CreateAcc.TabIndex = 22;
+            this.CreateAcc.TabIndex = 14;
             this.CreateAcc.Text = "Create an Account";
             this.CreateAcc.UseVisualStyleBackColor = true;
-            this.CreateAcc.Click += new System.EventHandler(this.CreateAcc_Click_1);
             // 
             // LoginTitle
             // 
             this.LoginTitle.AutoSize = true;
             this.LoginTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
-            this.LoginTitle.Location = new System.Drawing.Point(480, 155);
+            this.LoginTitle.Location = new System.Drawing.Point(453, 187);
             this.LoginTitle.Name = "LoginTitle";
             this.LoginTitle.Size = new System.Drawing.Size(212, 82);
-            this.LoginTitle.TabIndex = 21;
+            this.LoginTitle.TabIndex = 13;
             this.LoginTitle.Text = "Login";
             // 
             // loginConfirm
             // 
             this.loginConfirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.loginConfirm.Location = new System.Drawing.Point(535, 569);
+            this.loginConfirm.Location = new System.Drawing.Point(508, 601);
             this.loginConfirm.Name = "loginConfirm";
             this.loginConfirm.Size = new System.Drawing.Size(165, 57);
-            this.loginConfirm.TabIndex = 20;
+            this.loginConfirm.TabIndex = 12;
             this.loginConfirm.Text = "Login";
             this.loginConfirm.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.textBox1.Location = new System.Drawing.Point(472, 366);
+            this.textBox1.Location = new System.Drawing.Point(445, 398);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(307, 48);
-            this.textBox1.TabIndex = 19;
+            this.textBox1.TabIndex = 11;
             // 
             // Username
             // 
             this.Username.AutoSize = true;
             this.Username.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.Username.Location = new System.Drawing.Point(462, 323);
+            this.Username.Location = new System.Drawing.Point(435, 355);
             this.Username.Name = "Username";
             this.Username.Size = new System.Drawing.Size(183, 40);
-            this.Username.TabIndex = 18;
+            this.Username.TabIndex = 10;
             this.Username.Text = "Username";
             // 
             // Password
             // 
             this.Password.AutoSize = true;
             this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.Password.Location = new System.Drawing.Point(465, 427);
+            this.Password.Location = new System.Drawing.Point(438, 459);
             this.Password.Name = "Password";
             this.Password.Size = new System.Drawing.Size(175, 40);
-            this.Password.TabIndex = 17;
+            this.Password.TabIndex = 9;
             this.Password.Text = "Password";
             // 
             // PasswordTypeBox
             // 
             this.PasswordTypeBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
-            this.PasswordTypeBox.Location = new System.Drawing.Point(472, 478);
+            this.PasswordTypeBox.Location = new System.Drawing.Point(445, 510);
             this.PasswordTypeBox.Name = "PasswordTypeBox";
             this.PasswordTypeBox.Size = new System.Drawing.Size(307, 48);
-            this.PasswordTypeBox.TabIndex = 16;
+            this.PasswordTypeBox.TabIndex = 8;
             // 
-            // userControl21
-            // 
-            this.userControl21.BackColor = System.Drawing.Color.CadetBlue;
-            this.userControl21.Location = new System.Drawing.Point(0, -2);
-            this.userControl21.Name = "userControl21";
-            this.userControl21.Size = new System.Drawing.Size(1256, 972);
-            this.userControl21.TabIndex = 1;
-            // 
-            // userControl11
-            // 
-            this.userControl11.BackColor = System.Drawing.Color.Chocolate;
-            this.userControl11.Location = new System.Drawing.Point(0, -2);
-            this.userControl11.Name = "userControl11";
-            this.userControl11.Size = new System.Drawing.Size(1256, 972);
-            this.userControl11.TabIndex = 0;
-            // 
-            // Form1
+            // login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1255, 961);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.userControl21);
-            this.Controls.Add(this.userControl11);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.ClientSize = new System.Drawing.Size(1201, 1027);
+            this.Controls.Add(this.accQuestion);
+            this.Controls.Add(this.CreateAcc);
+            this.Controls.Add(this.LoginTitle);
+            this.Controls.Add(this.loginConfirm);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.PasswordTypeBox);
+            this.Name = "login";
+            this.Text = "login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private UserControl1 userControl11;
-        private UserControl2 userControl21;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label accQuestion;
         private System.Windows.Forms.Button CreateAcc;
         private System.Windows.Forms.Label LoginTitle;
@@ -181,4 +146,3 @@
         private System.Windows.Forms.TextBox PasswordTypeBox;
     }
 }
-
