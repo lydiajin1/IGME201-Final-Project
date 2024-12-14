a1861a6ca45de9ce0a679a62947d25aaaabdbe1f
Gavin <111817327+gavinwebst@users.noreply.github.com>
2024-12-14 18:03:59 -0500

    added a FoodItem class and DrinkItem subclass

---
+++ IGME201_Final_Project/DrinkItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGME201_Final_Project
+{
+    internal class DrinkItem : FoodItem
+    {
+        string size;
+
+        public DrinkItem(string size,int price, string name, string type) : base(price, name, type)
+        {
+            this.size = size;
+        }
+    }
+
+    
+
+}
---
+++ IGME201_Final_Project/foodItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGME201_Final_Project
+{
+    public class FoodItem
+    {
+        /*Variables for the FoodItem class */
+        public int price;
+        public string name;
+        public string type;
+        public int quantity = 0;
+        
+        /*constructor for the FoodItem class asking for the price, name, and type of the food item. */
+        public FoodItem(int price, string name, string type) {
+            this.price = price;
+            this.name = name;
+            this.type = type;
+        
+        }
+
+        /*get methods */
+        public int getPrice() { return price; }
+        public string getName() { return name; }
+        public string getType() { return type; }
+        public int getQuantity() { return quantity; }
+
+
+        /*set methods*/
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+
+        public void setPrice(int price) { this.price = price; }
+
+        public void setType(string type) { this.type = type; }
+
+        public void setName(string name) { this.name = name; }
+
+
+
+    }
+}
