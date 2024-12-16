c73d8040ee28fccff950249cf0fffa1849880532
Jacob Jablon (RIT Student) <jacob.r.jablon@gmail.com>
2024-12-15 21:50:26 -0500

    Update UserControl2.Designer.cs

--- IGME201_Final_Project/UserControl2.Designer.cs
+++ IGME201_Final_Project/UserControl2.Designer.cs
@@ -29,38 +29,37 @@
         private void InitializeComponent()
         {
             this.MenuTitle = new System.Windows.Forms.Label();
-            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
-            this.EntreesLabel = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.hamburgerContainer = new System.Windows.Forms.Panel();
             this.HamburgerAddBtn = new System.Windows.Forms.Button();
             this.HamburgerPrice = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.HamburgerLabel = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.MenuItemsContainer = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label5 = new System.Windows.Forms.Label();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.button2 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.E = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
             this.hamburgerContainer.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.MenuItemsContainer.SuspendLayout();
-            this.panel5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // MenuTitle
@@ -75,23 +74,18 @@
             this.MenuTitle.Text = "Menu";
             this.MenuTitle.Click += new System.EventHandler(this.MenuTitle_Click);
             // 
-            // vScrollBar1
+            // flowLayoutPanel1
             // 
-            this.vScrollBar1.Location = new System.Drawing.Point(1122, 0);
-            this.vScrollBar1.Name = "vScrollBar1";
-            this.vScrollBar1.Size = new System.Drawing.Size(31, 658);
-            this.vScrollBar1.TabIndex = 0;
-            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
-            // 
-            // EntreesLabel
-            // 
-            this.EntreesLabel.AutoSize = true;
-            this.EntreesLabel.Font = new System.Drawing.Font("Berlin Sans FB", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.EntreesLabel.Location = new System.Drawing.Point(3, 0);
-            this.EntreesLabel.Name = "EntreesLabel";
-            this.EntreesLabel.Size = new System.Drawing.Size(195, 61);
-            this.EntreesLabel.TabIndex = 1;
-            this.EntreesLabel.Text = "Entrees";
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.flowLayoutPanel1.Controls.Add(this.hamburgerContainer);
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Controls.Add(this.panel3);
+            this.flowLayoutPanel1.Controls.Add(this.E);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(60, 170);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1134, 652);
+            this.flowLayoutPanel1.TabIndex = 32;
             // 
             // hamburgerContainer
             // 
@@ -100,10 +94,10 @@
             this.hamburgerContainer.Controls.Add(this.HamburgerPrice);
             this.hamburgerContainer.Controls.Add(this.pictureBox1);
             this.hamburgerContainer.Controls.Add(this.HamburgerLabel);
-            this.hamburgerContainer.Location = new System.Drawing.Point(138, 91);
+            this.hamburgerContainer.Location = new System.Drawing.Point(3, 3);
             this.hamburgerContainer.Name = "hamburgerContainer";
             this.hamburgerContainer.Size = new System.Drawing.Size(371, 509);
-            this.hamburgerContainer.TabIndex = 2;
+            this.hamburgerContainer.TabIndex = 3;
             // 
             // HamburgerAddBtn
             // 
@@ -144,17 +138,17 @@
             this.HamburgerLabel.TabIndex = 0;
             this.HamburgerLabel.Text = "Hamburger";
             // 
-            // panel3
+            // panel1
             // 
-            this.panel3.BackColor = System.Drawing.SystemColors.Control;
-            this.panel3.Controls.Add(this.button1);
-            this.panel3.Controls.Add(this.label1);
-            this.panel3.Controls.Add(this.pictureBox2);
-            this.panel3.Controls.Add(this.label2);
-            this.panel3.Location = new System.Drawing.Point(624, 91);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(371, 509);
-            this.panel3.TabIndex = 3;
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(380, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(371, 509);
+            this.panel1.TabIndex = 4;
             // 
             // button1
             // 
@@ -195,82 +189,17 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Hamburger";
             // 
-            // MenuItemsContainer
+            // panel2
             // 
-            this.MenuItemsContainer.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.MenuItemsContainer.Controls.Add(this.panel5);
-            this.MenuItemsContainer.Controls.Add(this.panel4);
-            this.MenuItemsContainer.Controls.Add(this.panel3);
-            this.MenuItemsContainer.Controls.Add(this.hamburgerContainer);
-            this.MenuItemsContainer.Controls.Add(this.EntreesLabel);
-            this.MenuItemsContainer.Controls.Add(this.vScrollBar1);
-            this.MenuItemsContainer.Location = new System.Drawing.Point(49, 169);
-            this.MenuItemsContainer.Name = "MenuItemsContainer";
-            this.MenuItemsContainer.Size = new System.Drawing.Size(1154, 658);
-            this.MenuItemsContainer.TabIndex = 32;
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.SystemColors.Control;
-            this.panel5.Controls.Add(this.button3);
-            this.panel5.Controls.Add(this.label5);
-            this.panel5.Controls.Add(this.pictureBox4);
-            this.panel5.Controls.Add(this.label6);
-            this.panel5.Location = new System.Drawing.Point(624, 658);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(371, 509);
-            this.panel5.TabIndex = 5;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(92, 423);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(174, 67);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Add to Cart";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Berlin Sans FB", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(135, 366);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(106, 44);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "$7.99";
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Image = global::IGME201_Final_Project.Properties.Resources.hamburger_img;
-            this.pictureBox4.Location = new System.Drawing.Point(34, 63);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(300, 300);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Berlin Sans FB", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(74, 13);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(214, 44);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "Hamburger";
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.SystemColors.Control;
-            this.panel4.Controls.Add(this.button2);
-            this.panel4.Controls.Add(this.label3);
-            this.panel4.Controls.Add(this.pictureBox3);
-            this.panel4.Controls.Add(this.label4);
-            this.panel4.Location = new System.Drawing.Point(138, 658);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(371, 509);
-            this.panel4.TabIndex = 4;
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(757, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(371, 509);
+            this.panel2.TabIndex = 5;
             // 
             // button2
             // 
@@ -311,29 +240,89 @@
             this.label4.TabIndex = 0;
             this.label4.Text = "Hamburger";
             // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Control;
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.pictureBox4);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Location = new System.Drawing.Point(3, 518);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(371, 509);
+            this.panel3.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(92, 423);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 67);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Add to Cart";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Berlin Sans FB", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(135, 366);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 44);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "$7.99";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::IGME201_Final_Project.Properties.Resources.hamburger_img;
+            this.pictureBox4.Location = new System.Drawing.Point(34, 63);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Berlin Sans FB", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(74, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(214, 44);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Hamburger";
+            // 
+            // E
+            // 
+            this.E.AutoSize = true;
+            this.E.Location = new System.Drawing.Point(380, 515);
+            this.E.Name = "E";
+            this.E.Size = new System.Drawing.Size(78, 24);
+            this.E.TabIndex = 7;
+            this.E.Text = "Entrees";
+            // 
             // UserControl2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Chocolate;
-            this.Controls.Add(this.MenuItemsContainer);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.MenuTitle);
             this.Name = "UserControl2";
             this.Size = new System.Drawing.Size(1256, 871);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.hamburgerContainer.ResumeLayout(false);
             this.hamburgerContainer.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.MenuItemsContainer.ResumeLayout(false);
-            this.MenuItemsContainer.PerformLayout();
-            this.panel5.ResumeLayout(false);
-            this.panel5.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -341,28 +330,27 @@
 
         #endregion
         private System.Windows.Forms.Label MenuTitle;
-        private System.Windows.Forms.VScrollBar vScrollBar1;
-        private System.Windows.Forms.Label EntreesLabel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Panel hamburgerContainer;
         private System.Windows.Forms.Button HamburgerAddBtn;
         private System.Windows.Forms.Label HamburgerPrice;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label HamburgerLabel;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel MenuItemsContainer;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label E;
     }
 }
